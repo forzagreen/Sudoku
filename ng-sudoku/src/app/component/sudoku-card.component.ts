cb4fbaa553f7a4236006315b04d9ed386223dcfd
A Boujane <abe.boujane@gmail.com>
2017-05-29 05:22:34 -0400

    added input validation

--- ng-sudoku/src/app/component/sudoku-card.component.ts
+++ ng-sudoku/src/app/component/sudoku-card.component.ts
@@ -10,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class SudokuCard implements OnInit {
     dimension: number = 3;
     //debuggingPurposes
-    grid: Array<Array<number>>=[[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],
-    [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],
-    [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]]
+    grid: Array<Array<number>>=[
+        [,2,,6,,8,,,,],
+        [5,8,,,,9,7,,,],
+        [3,7,,,,,5,,,],
+        [,,,,4,,,,,],
+        [6,,,,,,,,4,],
+        [,,8,,,,,1,3,],
+        [,,,,2,,,,,],
+        [,,9,8,,,,3,6,],
+        [,,9,8,,6,,9,,]]
 
     constructor() { }
 
@@ -21,4 +28,12 @@ export class SudokuCard implements OnInit {
     trackByIndex(index: number, obj: any): any {
         return index;
   }
+    onKey(event:any, indexy:number,index:number){
+        // Get ASCII code of character
+        let value = event.key.charCodeAt(0);
+        // verify if input is whithin 1-9
+        if(value<49 || value>58){
+            this.grid[indexy][index]=undefined;
+        }
+    }
 }
