f34d38a07f8bdd07ad55cbb606a99cfc4da3f2ea
A Boujane <abe.boujane@gmail.com>
2017-05-29 14:08:33 -0400

    Added Initialization method to create the grid object - with Value and AllowedValues for each cell

--- ng-sudoku/src/app/component/sudoku-card.component.ts
+++ ng-sudoku/src/app/component/sudoku-card.component.ts
@@ -10,16 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class SudokuCard implements OnInit {
     dimension: number = 3;
     //debuggingPurposes
-    grid: Array<Array<number>>=[
-        [,2,,6,,8,,,,],
-        [5,8,,,,9,7,,,],
-        [3,7,,,,,5,,,],
-        [,,,,4,,,,,],
-        [6,,,,,,,,4,],
-        [,,8,,,,,1,3,],
-        [,,,,2,,,,,],
-        [,,9,8,,,,3,6,],
-        [,,9,8,,6,,9,,]]
+    grid: Array<Array<number>> = [
+        [, 2, , 6, , 8, , , ,],
+        [5, 8, , , , 9, 7, , ,],
+        [3, 7, , , , , 5, , ,],
+        [, , , , 4, , , , ,],
+        [6, , , , , , , , 4,],
+        [, , 8, , , , , 1, 3,],
+        [, , , , 2, , , , ,],
+        [, , 9, 8, , , , 3, 6,],
+        [, , 9, 8, , 6, , 9, ,]];
+
+    gridObj: Array<Array<Object>>;
 
     constructor() { }
 
@@ -27,13 +29,37 @@ export class SudokuCard implements OnInit {
 
     trackByIndex(index: number, obj: any): any {
         return index;
-  }
-    onKey(event:any, indexy:number,index:number){
+    }
+    onKey(event: any, indexy: number, index: number) {
         // Get ASCII code of character
         let value = event.key.charCodeAt(0);
         // verify if input is whithin 1-9
-        if(value<49 || value>58){
-            this.grid[indexy][index]=undefined;
+        if (value < 49 || value > 58) {
+            this.grid[indexy][index] = undefined;
         }
+        this.initialize();
     }
+
+    initialize() {
+        //Initialize GridObject 
+        this.gridObj = this.grid.map(
+            line => {
+                let lineArray = [];
+                for (let i = 0; i < 9; i++) {
+                    lineArray[i] = {
+                        value: line[i],
+                        allowedValues: (line[i] ? undefined : new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+                    }
+                }
+                return lineArray;
+            });
+        
+    }
+
+    updateAllowedValues(){
+        this.gridObj.map(lineArray=>{
+            console.log("updateAllowedValues");
+        });
+    }
+
 }
