b0b40607a2c1e3e7ae65fc53fab95d94eef529d9
A Boujane <abe.boujane@gmail.com>
2017-06-09 02:31:27 -0400

    Added camera support - On desktop - still some issues with Mobile - requires https

--- ng-sudoku/src/app/component/camera-dialog.component.ts
+++ ng-sudoku/src/app/component/camera-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, ViewChildren, Component, OnInit } from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CameraDialog implements OnInit {
+
+    @ViewChild('inputVideo') inputVideo: any;
+    @ViewChild('snap') snap: any;
+    @ViewChild('canvas') canvas: any;
+
+    mediaStream: any;
+    photoTaken: boolean = false;
+
     constructor() { }
     ngOnInit() {
-        
+        this.photoTaken = false;
+        let video = this.inputVideo.nativeElement;
+        let n = <any>navigator;
+        n.getUserMedia = (n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia);
+        n.mediaDevices.getUserMedia({
+            video: {
+                facingMode: { exact: "environment" }
+            }
+        }).then((stream) => {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            this.mediaStream = stream.getTracks()[0];
+        }).catch(() => window.alert("Sorry! Something went wrong!"));
     }
+
+    stopVideo() {
+        this.mediaStream.stop();
+    }
+
+    snapPic() {
+        this.photoTaken = true;
+        let img = this.snap.nativeElement;
+        let canvas = this.canvas.nativeElement;
+        let video = this.inputVideo.nativeElement;
+        let context;
+        let width = video.offsetWidth
+            , height = video.offsetHeight;
+        canvas.width = width;
+        canvas.height = height;
+
+        context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, width, height);
+
+        img.src = canvas.toDataURL('image/png');
+        this.mediaStream.stop();
+        video.style.display = 'none';
+    };
 }
 
